02a8a793fdb1e1d83e0c2de90718f3d369f93ad9
Rushing <liming20110711@163.com>
2019-03-29 00:21:58 +0800

    update:20190329-01

---
+++ sell-product-server/src/main/java/com/coding/sell/controller/ServerController.java
@@ -0,0 +1,27 @@
+/*
+ * 文件名称：ServerController.java
+ * 系统名称：[系统名称]
+ * 模块名称：[模块名称]
+ * 软件版权：Copyright (c) 2011-2019, liming20110711@163.com All Rights Reserved.
+ * 功能说明：[请在此处输入功能说明]
+ * 开发人员：Rushing0711
+ * 创建日期：20190328 23:12
+ * 修改记录：
+ * <Version>        <DateSerial>        <Author>        <Description>
+ * 1.0.0            20190328-01         Rushing0711     M201903282312 新建文件
+ ********************************************************************************/
+package com.coding.sell.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ServerController {
+
+    @GetMapping("/msg")
+    public String msg() {
+        return "this is product' msg";
+    }
+}
